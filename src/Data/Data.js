be026e87102082a46b930eadcbbf6ed91527a77e
chaima soltani <azakiachaima123456@gmail.com>
2023-10-30 12:39:47 +0100

    first commit

--- src/Data/Data.js
+++ src/Data/Data.js
@@ -6,6 +6,7 @@ const data =[
     img: "https://m.media-amazon.com/images/M/MV5BMDg0ZDE4MmItZDNlNS00YWYyLWEzNGEtM2Q4YjNlNDdjNmYzXkEyXkFqcGdeQXVyMTMxMTY0OTQ@._V1_.jpg",
     description:"Description for Dabbe 6",
     rate: 4.5,
+    trailerLink:"https://www.youtube.com/embed/wLD7weUW0ps?si=RWH0VqIXKqgVchU8"
    
 },
 {
@@ -14,6 +15,7 @@ const data =[
     img: "https://images.store.sky.com/api/img/asset/en/66D8BB8A-E4E8-4422-9242-603110084545_A1A4BC0E-5B2C-4196-9BAE-3462F61D8DF3_2023-4-3-T8-27-45.jpg?s=260x371",
     description:"Description for Megan",
     rate: 3.8,
+    trailerLink:"https://www.youtube.com/embed/53VJOxXdKCI?si=m6Nu0torBbJQlyQ0"
 
    
 },
--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -17,8 +17,9 @@ const MovieDetails = ({ movies }) => {
   return (
     <div>
       <h2>{Movie.title}</h2>
-      <p>Description: {movies.description}</p>
-      <p>Rating: {movies.rating}</p>
+      <p>Description: {Movie.description}</p>
+      <p>Rating: {Movie.rating}</p>
+      <iframe width="560" height="315" src={Movie.trailerLink} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
       
       
       <Link to="/">Go Back</Link>
